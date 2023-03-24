a8e52c69b1d1707e1562cff05fcef0963ff89728
Peter Paprotny <pappet@web.de>
2023-03-24 22:30:30 +0100

    Changed the processing from a list into a set. List processing took nearly 9h. Set processing took only ~30 sec.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
-
+import csv
+import time
 
 def read_json(file_path):
     try:
@@ -83,7 +84,7 @@ def extract_key_paths(obj, current_path=None, paths=None):
     if isinstance(obj, dict):
         for key, value in obj.items():
             new_path = current_path + [key]
-            paths.append(new_path)
+            paths.append(tuple(new_path))
             extract_key_paths(value, new_path, paths)
     elif isinstance(obj, list):
         for item in obj:
@@ -92,20 +93,23 @@ def extract_key_paths(obj, current_path=None, paths=None):
     return paths
 
 
-if __name__ == '__main__':
-    folder_path = '/home/peter/Schreibtisch/JSONS'
-    output_folder_name = 'output'
+def find_list_keys(data, path=''):
+    list_keys = []
 
-    # Erstelle den Pfad für den Ausgabeordner
-    output_folder_path = os.path.join(folder_path, output_folder_name)
-    # Erstelle den Ausgabeordner, falls er noch nicht existiert
-    if not os.path.exists(output_folder_path):
-        os.makedirs(output_folder_path)
+    if isinstance(data, dict):
+        for key, value in data.items():
+            new_path = f"{path} -> {key}" if path else key
+            result = find_list_keys(value, new_path)
+            list_keys.extend(result)
+    elif isinstance(data, list):
+        return [path]
 
+    return list_keys
+
+
+def folder_json(data_list, data_list_combined):
     # Liste der Dateien im Ordner
     file_list = os.listdir(folder_path)
-    # Kombinierte Daten speichern
-    combined_data = []
     # Durch die Dateien im Ordner iterieren
     for index, filename in enumerate(file_list):
         # Dateipfad erstellen
@@ -133,13 +137,13 @@ if __name__ == '__main__':
             write_txt(txt_path, unique_key_paths)
 
             if index == 0:
-                combined_data = key_paths
+                data_list = key_paths
             else:
                 # Vergleiche die Werte für jeden Index in der Liste und speichere den maximalen Wert
                 for key in key_paths:
-                    combined_data.append(key)
+                    data_list.append(key)
 
-            formatted_combined_data = [' -> '.join(path) for path in combined_data]
+            formatted_combined_data = [' -> '.join(path) for path in data_list]
             unique_combined_data = list(set(formatted_combined_data))
 
             # Speichere die kombinierten Daten in einer neuen Datei
@@ -147,3 +151,84 @@ if __name__ == '__main__':
             combined_data_path = os.path.join(output_folder_path, combined_data_filename)
 
             write_txt(combined_data_path, unique_combined_data)
+
+            list_keys = find_list_keys(loaded_json)
+            if index == 0:
+                data_list_combined = list_keys
+            else:
+                # Vergleiche die Werte für jeden Index in der Liste und speichere den maximalen Wert
+                for key in list_keys:
+                    data_list_combined.append(key)
+
+            unique_combined_list_keys = list(set(data_list_combined))
+            list_keys_filename = "list_keys.txt"
+            list_keys_path = os.path.join(output_folder_path, list_keys_filename)
+            write_txt(list_keys_path, unique_combined_list_keys)
+
+
+def csv_json(data_list, data_list_combined):
+    csv_file_path = '/home/peter/Schreibtisch/JSONS/XXX.csv'
+    previous_list_keys = set()
+    previous_combined_set = set()
+
+    list_keys_filename = "list_keys.txt"
+    list_keys_path = os.path.join(output_folder_path, list_keys_filename)
+    # Speichere die kombinierten Daten in einer neuen Datei
+    combined_data_filename = "combined_data.txt"
+    combined_data_path = os.path.join(output_folder_path, combined_data_filename)
+
+    with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+
+        for index, row in enumerate(reader):
+            json_data_str = row['content']
+            json_data = json.loads(json_data_str)
+
+            list_keys = find_list_keys(json_data)
+            if index == 0:
+                data_list_combined = set(list_keys)
+            else:
+                data_list_combined.update(list_keys)
+
+            unique_combined_list_keys = data_list_combined
+
+            if unique_combined_list_keys != previous_list_keys:
+                print(index)
+                previous_list_keys = unique_combined_list_keys
+
+            key_paths = extract_key_paths(json_data)
+            if index == 0:
+                data_list = set(key_paths)
+            else:
+                data_list.update(key_paths)
+
+            formatted_combined_data = {' -> '.join(path) for path in data_list}
+
+            if formatted_combined_data != previous_combined_set:
+                print(index)
+                previous_combined_set = formatted_combined_data
+
+    write_txt(list_keys_path, sorted(unique_combined_list_keys))
+    write_txt(combined_data_path, sorted(previous_combined_set))
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    folder_path = '/home/XXX/JSONS'
+
+    output_folder_name = 'output2'
+    # Erstelle den Pfad für den Ausgabeordner
+    output_folder_path = os.path.join(folder_path, output_folder_name)
+    # Erstelle den Ausgabeordner, falls er noch nicht existiert
+    if not os.path.exists(output_folder_path):
+        os.makedirs(output_folder_path)
+    # Kombinierte Daten speichern
+    combined_data = []
+    combined_list_keys = []
+
+    csv_json(combined_data, combined_list_keys)
+    # folder_json(combined_data, combined_list_keys)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"The program took {elapsed_time:.2f} seconds to run.")
